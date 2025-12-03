a0d0b3cb03c8f4385042ff37aad0f78e0a02dae0
beingglitch <surajshukla5604@gmail.com>
2025-12-03 05:49:01 +0530

    chore: Page Progress bar

--- components/landing_page/Navigation.tsx
+++ components/landing_page/Navigation.tsx
@@ -101,7 +101,7 @@ export default function Navigation() {
 
         {/* Scroll progress indicator */}
         <motion.div
-          className="absolute bottom-0 left-0 right-0 h-0.5 bg-linear-to-r from-primary via-purple-600 to-secondary origin-left"
+          className="absolute bottom-0 left-0 right-0 h-0.5 bg-linear-to-r from-primary to-secondary origin-left"
           style={{ scaleX: scrollYProgress }}
         />
       </motion.nav>
